19d9edab2471512a7ded42d012bf06eaacb9a388
Veon2479 <76491129+Veon2479@users.noreply.github.com>
2021-11-19 22:13:51 +0300

    lab3 finished

--- l3/Queue.pas
+++ l3/Queue.pas
@@ -18,14 +18,78 @@ type
 var
   Head: tAdr;
   prior: array[1..3] of tPrior = ([1, 2, 3], [4, 5], [6, 7, 8]);
-  active: array[1..8] of integer = (0, 0, 0, 0, 0, 0, 0, 0);
+  users: array[1..8] of tAr=((3, 2, 1, 6, 3, 2, 5, 4, 0, 0, 0, 0),
+                              (2, 1, 2, 3, 3, 1, 2, 6, 4, 0, 0, 0),
+                              (4, 1, 6, 8, 5, 4, 2, 3, 1, 2, 0, 0),
+                              (2, 4, 8, 9, 3, 2, 1, 6, 5, 9, 0, 0),
+                              (9, 6, 7, 2, 3, 1, 4, 5, 1, 3, 2, 0),
+                              (2, 3, 2, 1, 6, 3, 4, 1, 2, 3, 1, 0),
+                              (3, 2, 3, 1, 3, 4, 1, 2, 1, 6, 1, 2),
+                              (2, 3, 3, 3, 1, 6, 3, 8, 4, 2, 2, 2));
+  busy: array[1..8] of integer = (0, 0, 0, 0, 0, 0, 0, 0);
 
-  function getPrior(const curUser: tUser): tUSer;
-  function getNextUser(const curUser: tUser): tUser;
+
+  procedure init;
+  function getNextUser: tUser;
+  function isEmpty: boolean;
+
+  function getUserTime(const USER: tUser): integer;
+  procedure decUserTime(const USER: tUser; const TIME: integer);
+
+  function getTaskSum: integer;
+
+  procedure decBusy(const TIME: integer);
+  procedure setBusy(const USER: tUser; const TIME: integer);
 
 implementation
 
+  procedure decBusy(const TIME: integer);
+    var
+      user: tUser;
+    begin
+      for user:=1 to 8 do
+        if busy[user]>TIME then dec(busy[user], TIME)
+            else busy[user]:=0;
+    end;
 
+  procedure setBusy(const USER: tUser; const TIME: integer);
+    begin
+      busy[USER]:=TIME;
+    end;
+
+  function isBusy(const USER: tUser): boolean;
+    begin
+      if busy[USER]<>0 then RESULT:=true
+          else RESULT:=false;
+    end;
+
+
+  function getTaskSum: integer;
+    var
+      user: tUSer;
+      i: integer;
+    begin
+      RESULT:=0;
+      for i:=1 to 12 do
+        for user:=1 to 8 do
+          inc(RESULT, users[user, i]);
+    end;
+
+  function getUserTime(const USER: tUser): integer;
+    var
+      j: integer;
+    begin
+      j:=1;
+      while (j<=12)and(users[USER, j]=0) do
+        inc(j);
+      RESULT:=users[USER, j];
+    end;
+
+  function isEmpty: boolean;
+    begin
+      if (Head^.next = nil) then RESULT:=true
+          else RESULT:=false;
+    end;
 
   function getPrior(const curUser: tUser): tUSer;
   var
@@ -35,37 +99,120 @@ implementation
       if (curUser IN prior[i]) then RESULT:=i;
   end;
 
-  function getNextUser(const curUser: tUser): tUser;   //*****
-    begin
-
-    end;
-
-  function pop: tUser; //******
-    begin
-
-    end;
-
-  procedure push(const USER: tUser); //******
+  function pop: tUser;
     var
-      adr: tAdr;
+      adr, tmp: tAdr;
+    begin
+                                     //список гарантированно не пуст
+      adr:=Head;
+      while (adr^.next<>nil) do
+        begin
+          if not isBusy(adr^.next.user) then
+              begin
+                RESULT:=adr^.next.user;
+                tmp:=adr;
+                break;
+              end;
+          adr:=adr^.next;
+        end;
+      if (adr^.next=nil) then RESULT:=0
+        else
+          begin
+            adr:=tmp^.next;
+            tmp^.next:=adr^.next;
+            dispose(adr);
+          end;
+    end;
+
+  procedure push(const USER: tUser);
+    var
+      adr, tmp: tAdr;
+      f: boolean;
     begin
       new(adr);
       adr^.user := USER;
 
-      if Head^.next = nil then Head^.next:=adr
-        else
-          begin
+      tmp:=Head;
+      if tmp^.next=nil then f:=false
+        else f:=true;
 
-          end;
 
+      while f do
+        begin
+          if tmp^.next=nil then f:=false
+            else if (getPrior(tmp^.next^.user)>getPrior(adr^.user)) then f:=false;
+          if f then tmp:=tmp^.next;
+        end;
+
+      adr^.next:=tmp^.next;
+      tmp^.next:=adr;
     end;
 
+  function isHaveJob(const USER: tUser): boolean;
+    var
+      i: integer;
+    begin
+      RESULT:=false;
+      for i:=1 to 12 do
+        if (users[USER, i]<>0) then RESULT:=true;
+    end;
 
+  procedure clearGarbage;
+    var
+      adr, tmp: tAdr;
+    begin
+      adr:=Head;
+      while adr^.next<>nil do
+        begin
+          if not isHaveJob(adr^.next^.user) then
+              begin
+
+                tmp:=adr^.next;
+                 writeln('WRN: GARBAGE: user ', tmp^.user);
+                adr^.next:=tmp^.next;
+                dispose(tmp);
+              end;
+          if adr^.next<>nil then adr:=adr^.next;
+        end;
+    end;
+
+  function getNextUser: tUser;
+    begin
+      clearGarbage;
+
+      //извлечь юзера
+      //если у него ещё есть работа - а она пока типа ещё есть - положить его назад
+
+      //контроль занятости проводит pop()
+      if not isEmpty then
+          RESULT:=pop
+        else RESULT:=0;
+
+      if RESULT<>0 then
+          push(RESULT);
+    end;
+
+  procedure init;
+  var
+    i: tUser;
+  begin
+    new(Head);
+    Head^.user := 1;
+    Head^.next := nil;
+    for i:=1 to 8 do
+      push(i);
+  end;
+
+  procedure decUserTime(const USER: tUser; const TIME: integer);
+    var
+      i: integer;
+    begin
+      i:=1;
+      while (i<=12)and(users[USER, i]=0) do
+        inc(i);
+      dec(users[USER, i], TIME);
+    end;
 
 end.
 
-Initialization
-  new(Head);
-  Head^.user := 1;
-  Head^.next := nil;
-end.
+
--- l3/l3.dpr
+++ l3/l3.dpr
@@ -10,55 +10,173 @@ uses
 
 var
   CurUser: tUser;
-  users: array[1..8] of tAr=((3, 2, 1, 6, 3, 2, 5, 4, 0, 0, 0, 0),
-                              (2, 1, 2, 3, 3, 1, 2, 6, 4, 0, 0, 0),
-                              (4, 1, 6, 8, 5, 4, 2, 3, 1, 2, 0, 0),
-                              (2, 4, 8, 9, 3, 2, 1, 6, 5, 9, 0, 0),
-                              (9, 6, 7, 2, 3, 1, 4, 5, 1, 3, 2, 0),
-                              (2, 3, 2, 1, 6, 3, 4, 1, 2, 3, 1, 0),
-                              (3, 2, 3, 1, 3, 4, 1, 2, 1, 6, 1, 2),
-                              (2, 3, 3, 3, 1, 6, 3, 8, 4, 2, 2, 2));
-
+  userTime, curTime, t, sum: integer;
+  eff: real;
 
   tact, proc: tTime;
   graph: array[1..8, 1..1000] of integer;
 
 
-function getTime: tTime;
-  var
-    tmp: integer;
-  begin
-    tmp:=11;
-    while (tmp<0)or(tmp>10) do
-      begin
-        write('>');
-        readln(tmp);
-        if (tmp<0)or(tmp>10) then writeln('Incorrect input!')
-          else RESULT:=tmp;
-      end;
-  end;
+  function getTime: tTime;
+    var
+      tmp: integer;
+    begin
+      tmp:=11;
+      while (tmp<0)or(tmp>10) do
+        begin
+          write('> ');
+          readln(tmp);
+          if (tmp<0)or(tmp>10) then writeln('Incorrect input!')
+            else RESULT:=tmp;
+        end;
+    end;
 
-function isEmpty: boolean;
-  var
-    i, j: integer;
-  begin
-    RESULT:=true;
-    for i := 1 to 8 do
-      for j := 1 to 12 do
-        if users[i, j]<>0 then RESULT:=false;
-  end;
+  procedure localInit;
+    var
+      i: tUser;
+      j: integer;
+    begin
+      init;
+      for i := 1 to 8 do
+        for j := 1 to 1000 do
+          graph[i, j]:=-1;
+      curTime:=1;
+    end;
+                  //0 - nothing, 1 - read, 2 - computing
+  procedure fillRead(const USER: tUser; const OFFSET, TIME: integer);
+    var
+      i: integer;
+      curUser: tUSer;
+    begin
+      for i := 1 to tact do
+        for curUser := 1 to 8 do
+          if (graph[curUser, OFFSET+i-1] = -1) then
+            if (USER=curUser) then
+                begin
+                  if (i<=TIME) then
+                      graph[curUser, OFFSET+i-1]:=1
+                    else graph[curUser, OFFSET+i-1]:=0;
+                end
+              else
+                begin
+                  graph[curUser, OFFSET+i-1]:=0;
+                end;
+    end;
 
-function getUserTime(const USER: tUser): integer;
-  begin
-    RESULT:=active[USER];
-  end;
+  procedure fillCompute(const USER: tUSer; const OFFSET: integer);
+    var
+      i: integer;
+    begin
+      for i := 1 to proc do
+        if (graph[USER, OFFSET+i-1]<>-1) and (graph[USER, OFFSET+i-1]<>0)  then
+                writeln('ERR: COMPUTING_NOT_NULL at (user, i, sym): (',USER,', ',OFFSET+i-1,', ',graph[USER, OFFSET+i-1],')')
+            else graph[USER, OFFSET+i-1]:=2;
+    end;
+
+  function isFree(const USER: tUser; const OFFSET: integer): boolean;
+    begin
+      if (graph[USER, OFFSET]<>-1) then RESULT:=false
+          else RESULT:=true;
+    end;
+
+  procedure normalize(var OFFSET: integer);
+    var
+      user: tUser;
+      i: integer;
+    begin
+      i:=0;
+      for user:=1 to 8 do
+        if (graph[user, OFFSET]<>-1) then inc(i);
+      if i<>0 then
+        begin
+          fillRead(user, OFFSET, 0);
+          inc(OFFSET, tact);
+        end;
+    end;
+
+  procedure saveGraph(const OFFSET: integer);
+    var
+      user: tUSer;
+      i: integer;
+      f: textFile;
+    begin
+      assignFile(f, 'graph.txt');
+      rewrite(f);
+
+      for user := 1 to 8 do
+        begin
+          for i := 1 to OFFSET-1 do
+            begin
+              write(f, graph[USER, i]);
+              if (i mod tact = 0) then write(f, ' ');
+
+            end;
+          writeln(f);
+        end;
+      closeFIle(F);
+
+    end;
+
+  function validate(const OFFSET: integer): boolean;
+    var
+      user: tUser;
+      i, k: integer;
+    begin
+      RESULT:=true;
+      for i := 1 to OFFSET-1 do
+        begin
+          k:=0;
+          if graph[user, i]=1 then inc(k);
+        end;
+      if k>1 then
+        begin
+          writeln('ERR: MULTI_INPUT at (user, i): (',user,', ',i,')');
+          RESULT:=false;
+        end;
+    end;
+
 
 begin
   writeln('Enter tact time and processing time:');
+  sum:=getTaskSum;
   tact:=getTime;
   proc:=getTime;
+  localInit;
+  while not isEmpty do
+    begin
+      curUser:=getNextUser;
+      if curUser<>0 then
+        begin
+              userTime:=getUserTime(curUser);
 
+              if userTime<=0 then writeln('ERR: ZERO_USER_TIME at (user, i): (',curUser,', ',curTime,')');
 
-
-
+              if (userTime<=tact) then
+                  begin
+                    fillRead(curUser, curTime, userTime);
+                    fillCompute(curUser, curTime+userTime);
+                    decUserTime(curUSer, userTime);
+                    setBusy(curUser, userTime+proc);
+                  end
+                else
+                  begin
+                    fillRead(curUser, curTime, tact);
+                    decUserTime(curUSer, tact);
+                    setBusy(curUser, tact);
+                  end;
+        end
+        else
+          fillRead(curUser, curTime, 0);
+      inc(curTime, tact);
+      decBusy(tact);
+    end;
+  normalize(curTime);
+  saveGraph(curTime);
+  validate(curTime);
+  eff:=sum/curTime;
+  writeln('all time is: ',curTime);
+  writeln('sum of time of tasks: ',sum);
+  writeln('ÊÏÄ = ',eff:6:4);
+  writeln('..');
+  readln;
 end.
